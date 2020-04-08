5d45b3360116925e78908706d37b7d4928a39411
viccom <3520845@qq.com>
2020-04-08 10:29:16 +0800

    add

--- erp2thingscloud/erp2thingscloud/doctype/thingscloud_settings/thingscloud_settings.js
+++ erp2thingscloud/erp2thingscloud/doctype/thingscloud_settings/thingscloud_settings.js
@@ -3,6 +3,37 @@
 
 frappe.ui.form.on('ThingsCloud Settings', {
 	// refresh: function(frm) {
+	setup: function(frm) {
+		frm.events.refresh_status(frm);
+	},
+	refresh: function(frm) {
+		frm.add_custom_button(__("Refresh Server Status"), function() {
+			frm.events.refresh_status(frm);
+		}).removeClass("btn-default").addClass("btn-primary");
 
+		var grid_html = '<div class="form-group"> \
+							<div class="clearfix"> \
+								<label class="control-label" style="padding-right: 0px;">%(title)s</label> \
+							</div> \
+							<div class="control-input-wrapper"> \
+								<img height="32px" src="/assets/erp2thingscloud/images/connect/%(status)s.png"> \
+							</div> \
+						</div>'
+
+		var thingscloud_status = frm.doc.thingscloud_status  || 'none';
+		var s = $(repl(grid_html, {title: __("ThingsCloud"), status: thingscloud_status.toLowerCase()}));
+		$(frm.fields_dict['server_status_html'].wrapper).append(s);
+
+	},
+	refresh_status: function(frm) {
+		return frappe.call({
+			doc: frm.doc,
+			method: "refresh_status",
+			freeze: true,
+			callback: function(r) {
+				if(!r.exc) frm.reload_doc();
+			}
+		})
+	}
 	// }
 });
--- erp2thingscloud/erp2thingscloud/doctype/thingscloud_settings/thingscloud_settings.py
+++ erp2thingscloud/erp2thingscloud/doctype/thingscloud_settings/thingscloud_settings.py
@@ -3,8 +3,56 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+import re
+import requests
+
 
 class ThingsCloudSettings(Document):
+	def update_thingscloud_status(self, status):
+		self.thingscloud_status = status
+		self.thingscloud_updated = frappe.utils.now()
+		self.save()
+
+	def refresh_status(self):
+		status = get_thingscloud_status()
+		if status:
+			self.update_thingscloud_status("ON")
+		else:
+			self.update_thingscloud_status("OFF")
+
+	@staticmethod
+	def get_thingscloud_server():
+		url = frappe.db.get_single_value("ThingsCloud Settings", "erp2thingscloud_url")
+		if not url:
+			return None
+		return gen_server_url(url, "http", 80)
 	pass
+
+
+def gen_server_url(server, protocol, port):
+	m = re.search("^(.+)://(.+)$", server)
+	if m:
+		protocol = m.group(1)
+		server = m.group(2)
+
+	m = re.search("^(.+):(\d+)$", server)
+
+	if m:
+		server = m.group(1)
+		port = m.group(2)
+	return ("{0}://{1}:{2}").format(protocol, server, port)
+
+
+def get_thingscloud_status():
+	try:
+		inf_server = ThingsCloudSettings.get_thingscloud_server()
+		if not inf_server:
+			frappe.logger(__name__).error("ThingsCloud Configuration missing in ThingsCloudSettings")
+			return
+
+		r = requests.session().get(inf_server + "/query", params={"q": '''SHOW USERS'''}, timeout=1)
+		return r.status_code == 200
+	except Exception:
+		return False
