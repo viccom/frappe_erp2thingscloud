2359ecf980147c0add7aeeb46bf21ee7773ec975
viccom <3520845@qq.com>
2020-04-09 09:36:33 +0000

    v2

---
+++ erp2thingscloud/erp2thingscloud/doctype/serial_number_list/serial_number_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, viccom.dong and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Serial Number list', {
+	// refresh: function(frm) {
+
+	// }
+});
